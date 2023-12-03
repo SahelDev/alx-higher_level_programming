a50cd1923efde527f158abbfdf2b50d48f9489b4
denzel84 <baderou@yahoo.fr>
2023-12-03 20:11:47 +0100

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,17 +3,13 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for i in range(len(my_list)):
         new_list.append(my_list[i])
-
     if idx < 0:
         """must returns copy of the original list"""
         result = new_list
-        
     elif idx > len(my_list) - 1:
         """returns a copy of the original list"""
         result = new_list
-    
     else:
         new_list[idx] = element
         result = new_list
-
     return result
