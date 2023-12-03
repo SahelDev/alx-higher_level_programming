514e42239a1a07a36fdb5912bc512cf31e7cc2ef
denzel84 <baderou@yahoo.fr>
2023-12-03 19:01:03 +0100

    Secure access to an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,10 @@
-def element_at(my_list, idx):
-
+#!/usr/bin/python3
+def element_at (my_list, idx):
     """This function retrieve an element from a list"""
-
     if idx < 0:
         result = None
     elif idx > len(my_list) - 1:
         result = None
     else:
         result = my_list[idx]
-
     return result
--- 0x03-python-data_structures/1-main.py
+++ 0x03-python-data_structures/1-main.py
@@ -3,6 +3,6 @@ element_at = __import__('1-element_at').element_at
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
-idx1 = 5
+idx1 = -4
 print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
 print("Element at index {:d} is {}".format(idx1, element_at(my_list, idx1)))
