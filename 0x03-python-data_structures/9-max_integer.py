b63397ff4fe16813b63b6b15fb93418e27bb8c2c
denzel84 <baderou@yahoo.fr>
2023-12-05 15:08:32 +0100

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in range(len(my_list) - 1):
+            print("is {:d} < {:d} ?".format(my_list[i], my_list[i+1]))
+            if my_list[i] > my_list[i+1]:
+                print("SWITCH")
+                temp = my_list[i+1]
+                my_list[i+1] = my_list[i]
+                my_list[i] = temp
+                print("Max : {:d}".format(my_list[i+1]))
+    return my_list[i+1]
