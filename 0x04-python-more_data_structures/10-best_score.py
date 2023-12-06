f5ad174cdca3e21a6eaef68fed2f9c134ef6ab9e
denzel84 <baderou@yahoo.fr>
2023-12-06 15:47:01 +0100

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary != {} and a_dictionary != None:
+    if a_dictionary != {} and a_dictionary is not None:
         lists = []
         for i in a_dictionary:
             lists.append(a_dictionary[i])
-
         maximun = max(lists)
         for i in a_dictionary:
             if a_dictionary[i] == maximun:
                 return i
-    
