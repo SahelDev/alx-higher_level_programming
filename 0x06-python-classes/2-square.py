1dc64ad4d3c5060ca4fb33ca38d0bba4cf543a0b
denzel84 <baderou@yahoo.fr>
2023-12-20 00:58:17 +0100

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this module defines a class Square with a private instance"""
+
+
+class Square:
+    """this class represent a square with private attribute size"""
+
+    def __init__(self, size=0):
+        """Class constructor"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
