bd626b25750903abc1a58b326f24f851446bfd3a
denzel84 <baderou@yahoo.fr>
2024-01-06 11:46:09 +0100

    1. Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+Rectangle = __import__('1-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
+
+{'_Rectangle__height': 4, '_Rectangle__width': 2}
+{'_Rectangle__height': 3, '_Rectangle__width': 10}
