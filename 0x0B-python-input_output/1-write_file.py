b4027904f411f5a8fc764395d70952541d29c9ea
denzel84 <baderou@yahoo.fr>
2024-01-10 00:08:59 +0100

    task01

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,14 @@
 
 
 def write_file(filename="", text=""):
-    """definition of func write_file"""
+     """Write `text` to `filename`
+
+    Args:
+        filename (str): file to write to
+        text (str): text to write into `filename`
+
+    Returns: number of characters written
+    """
     with open(filename, 'w', encoding="utf-8") as f:
         nc = f.write(text)
         return nc
