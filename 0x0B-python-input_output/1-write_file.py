ee28827a176ef1eedc689122869ca65c20bbc479
denzel84 <baderou@yahoo.fr>
2024-01-09 14:58:23 +0100

    task00-02

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,9 +3,8 @@
         that write a string to a text file and\
         returns the number of character written"""
 
+
 def write_file(filename="", text=""):
     """definition of func write_file"""
     with open(filename, 'w', encoding="utf-8") as f:
-        nc = f.write(text)
-
-    return nc
+        return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module with function append_write"""
 
+
 def append_write(filename="", text=""):
     """func append_write definition"""
     with open(filename, 'a', encoding="utf-8") as f:
