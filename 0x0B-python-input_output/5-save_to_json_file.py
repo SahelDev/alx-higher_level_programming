90a1269fb656c1e75abe90d19e72e46671d04bcf
denzel84 <baderou@yahoo.fr>
2024-01-10 00:50:18 +0100

    task05

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module define functionthat writes an object to a text file"""
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """function definition:\
+            args:
+            my_obj
+            filename
+            Return """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
