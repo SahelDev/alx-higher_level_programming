ffb102823dc05598c23a4952db966bf90231b5cd
denzel84 <baderou@yahoo.fr>
2024-01-10 14:30:19 +0100

    task01

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""This module contains math function add , to add\
+        integers"""
+
+def add_integer(a, b=98):
+    """Definition of function add_integer \
+            it's take two integers or float as input then\
+            output their sum"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+
+    return int(a) + int(b)
+
