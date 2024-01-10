0b3ac642e0aad1a1cd75dadcb8fb7b53536d2cb6
denzel84 <baderou@yahoo.fr>
2024-01-10 01:38:54 +0100

    task7-9

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module define funct to load, add, save"""
+
+
+import sys
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+FILENAME = 'add_item.json'
+
+if __name__ == '__main__':
+    try:
+        save_to_json_file(load_from_json_file(FILENAME) + argv[1:], FILENAME)
+    except (FileNotFoundError, ValueError):
+        save_to_json_file(argv[1:], FILENAME)
