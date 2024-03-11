b77faedabdd5155e9b71f2e5366340cd9629d1e1
denzel84 <baderou@yahoo.fr>
2024-03-11 23:20:40 +0100

    correction

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-
-let i = 0;
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (i = 0; i < myVar.length; i++) {
-  console.log(myVar[i]);
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
